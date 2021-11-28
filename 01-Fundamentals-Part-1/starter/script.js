9054577a4536f961711e5753134d642e14acf0f2
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-28 20:29:58 +0000

    finished practice assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -3,3 +3,10 @@ console.log(40 + 8 + 23 - 10);
 
 let js$ = "test";
 console.log(js$);
+
+let country = "US";
+let continent = "North America";
+let population = 99999999999999999999;
+console.log(country);
+console.log(continent);
+console.log(population);
