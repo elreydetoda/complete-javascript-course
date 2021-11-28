9f127bc1cfdffdec8f400fa283b3836368ae56ec
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-28 21:02:43 +0000

    finished data types lecture

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,12 +1,16 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
 let js$ = "test";
 console.log(js$);
 
+// variable assignment
 let country = "US";
 let continent = "North America";
 let population = 99999999999999999999n;
 console.log(country);
 console.log(continent);
 console.log(population);
+
+*/
