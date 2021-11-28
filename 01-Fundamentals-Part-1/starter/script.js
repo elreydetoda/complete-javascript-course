ab9bfbc0a7866162596559a76719c6c08aa52d48
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-28 22:42:04 +0000

    finished notes on template literals

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -30,71 +30,94 @@ const countryz = "US";
 // countryz = "bobby";
 
 // math ops
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSara = now - 2018;
-console.log(ageJonas, ageSara);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSara = now - 2018;
+// console.log(ageJonas, ageSara);
 
-console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
-// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+// console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// // 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
 
-const fName = "J";
-const lName = "S";
-console.log(fName + " " + lName);
+// const fName = "J";
+// const lName = "S";
+// console.log(fName + " " + lName);
 
-// assignment operators
-let x = 10 + 5; // 15
-x += 10; // x = x + 10 = 25
-x *= 4; // x = x * 4 = 100
-x++; // x = x + 1
-x--; // x = x - 1
-console.log(x);
+// // assignment operators
+// let x = 10 + 5; // 15
+// x += 10; // x = x + 10 = 25
+// x *= 4; // x = x * 4 = 100
+// x++; // x = x + 1
+// x--; // x = x - 1
+// console.log(x);
 
-population = 1000;
+// population = 1000;
 
-console.log(population / 2);
-population++;
-console.log(population);
-finPop = 6000000;
-averPop = 33000000;
-console.log(population > finPop);
-console.log(population > averPop);
-const description =
-  "Portugal is in Europe, and its 11 million people speak portuguese";
+// console.log(population / 2);
+// population++;
+// console.log(population);
+// finPop = 6000000;
+// averPop = 33000000;
+// console.log(population > finPop);
+// console.log(population > averPop);
+// const description =
+//   "Portugal is in Europe, and its 11 million people speak portuguese";
 
-// copmarison operators
+// // copmarison operators
 
-let z, y;
-z = y = 25 - 10 - 5;
-console.log(x, y);
+// let z, y;
+// z = y = 25 - 10 - 5;
+// console.log(x, y);
 
-// Coding challenge 1
-// *************************
-// data 1
-// *************************
+// // Coding challenge 1
+// // *************************
+// // data 1
+// // *************************
 
-const markWeightD1 = 78;
-const markHeightD1 = 1.69;
-const johnWeightD1 = 92;
-const johnHeightD1 = 1.95;
+// const markWeightD1 = 78;
+// const markHeightD1 = 1.69;
+// const johnWeightD1 = 92;
+// const johnHeightD1 = 1.95;
 
-const markBMID1 = markWeightD1 / markHeightD1 ** 2;
-const johnBMID1 = johnWeightD1 / johnHeightD1 ** 2;
-const markHigherBMID1 = markBMID1 > johnBMID1;
+// const markBMID1 = markWeightD1 / markHeightD1 ** 2;
+// const johnBMID1 = johnWeightD1 / johnHeightD1 ** 2;
+// const markHigherBMID1 = markBMID1 > johnBMID1;
 
-console.log(markBMID1, johnBMID1, markHigherBMID1);
+// console.log(markBMID1, johnBMID1, markHigherBMID1);
 
-// *************************
-// data 2
-// *************************
+// // *************************
+// // data 2
+// // *************************
 
-const markWeightD2 = 95;
-const markHeightD2 = 1.88;
-const johnWeightD2 = 85;
-const johnHeightD2 = 1.76;
+// const markWeightD2 = 95;
+// const markHeightD2 = 1.88;
+// const johnWeightD2 = 85;
+// const johnHeightD2 = 1.76;
 
-const markBMID2 = markWeightD2 / markHeightD2 ** 2;
-const johnBMID2 = johnWeightD2 / johnHeightD2 ** 2;
-const markHigherBMID2 = markBMID2 > johnBMID2;
+// const markBMID2 = markWeightD2 / markHeightD2 ** 2;
+// const johnBMID2 = johnWeightD2 / johnHeightD2 ** 2;
+// const markHigherBMID2 = markBMID2 > johnBMID2;
 
-console.log(markBMID2, johnBMID2, markHigherBMID2);
+// console.log(markBMID2, johnBMID2, markHigherBMID2);
+
+const fName = "jonas";
+const job = "teacher";
+const bYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + fName + ", a " + (year - bYear) + " years old " + job + "!";
+
+console.log(jonas);
+
+const jonasNew = `I'm ${fName}, a ${year - bYear} years old ${job}!`;
+console.log(jonasNew);
+
+// old way for multi-line string
+console.log("String with \n\
+muliple \n\
+lines");
+
+// new way
+console.log(`String
+multiple
+lines`);
