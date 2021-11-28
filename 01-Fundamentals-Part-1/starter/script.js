b1d1dfee87ba927665f03b9195c50545654f7c20
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-28 21:33:19 +0000

    finished notes for section

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -28,3 +28,26 @@ console.log(language);
 
 const countryz = "US";
 // countryz = "bobby";
+
+// math ops
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSara = now - 2018;
+console.log(ageJonas, ageSara);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const fName = "J";
+const lName = "S";
+console.log(fName + " " + lName);
+
+// assignment operators
+let x = 10 + 5; // 15
+x += 10; // x = x + 10 = 25
+x *= 4; // x = x * 4 = 100
+x++; // x = x + 1
+x--; // x = x - 1
+console.log(x);
+
+// copmarison operators
