85d15ca1849d241c054eae80837be1a471c2a2eb
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-28 21:04:18 +0000

    finished practice assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -5,6 +5,8 @@ console.log(40 + 8 + 23 - 10);
 let js$ = "test";
 console.log(js$);
 
+
+*/
 // variable assignment
 let country = "US";
 let continent = "North America";
@@ -13,4 +15,9 @@ console.log(country);
 console.log(continent);
 console.log(population);
 
-*/
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
