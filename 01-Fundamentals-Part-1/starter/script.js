8fc8a58edc292744552988b027b97d2d81bdb5f1
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2021-11-29 01:25:09 +0000

    finished practice assignment

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -345,46 +345,70 @@ console.log(js$);
 //   console.log("You're all loosers!");
 // }
 
-const day = "monday";
-// const day = "wednesday";
-// const day = "thursday";
-// const day = "never";
+// const day = "monday";
+// // const day = "wednesday";
+// // const day = "thursday";
+// // const day = "never";
 
-switch (day) {
-  case "monday":
-    console.log("Plan course structure");
-    console.log("Go to coding meetup");
+// switch (day) {
+//   case "monday":
+//     console.log("Plan course structure");
+//     console.log("Go to coding meetup");
+//     break;
+//   case "tuesday":
+//     console.log("Prepare theory videos");
+//     break;
+//   case "wednesday":
+//   case "thursday":
+//     console.log("Write code examples");
+//     break;
+//   case "friday":
+//     console.log("Record videos");
+//     break;
+//   case "saturday":
+//   case "sunday":
+//     console.log("Enjoy the weekend :D");
+//     break;
+//   default:
+//     console.log("Not a valid day!");
+//     break;
+// }
+
+// if (day === "monday") {
+//   console.log("Plan course structure");
+//   console.log("Go to coding meetup");
+// } else if (day === "tuesday") {
+//   console.log("Prepare theory videos");
+// } else if (day === "wednesday" || day === "thursday") {
+//   console.log("Write code examples");
+// } else if (day === "friday") {
+//   console.log("Record videos");
+// } else if (day === "saturday" || day === "sunday") {
+//   console.log("enjoy the weekend");
+// } else {
+//   console.log("Not a valid day!");
+// }
+
+const language = "english";
+
+switch (language) {
+  case "chinese":
+  case "mandarin":
+    console.log("MOST number of native speakers!");
     break;
-  case "tuesday":
-    console.log("Prepare theory videos");
+  case "spanish":
+    console.log("2nd place in number of native speakers");
     break;
-  case "wednesday":
-  case "thursday":
-    console.log("Write code examples");
+  case "english":
+    console.log("3rd place");
     break;
-  case "friday":
-    console.log("Record videos");
+  case "hindi":
+    console.log("Number 4");
     break;
-  case "saturday":
-  case "sunday":
-    console.log("Enjoy the weekend :D");
+  case "arabic":
+    console.log("5th most spoken language");
     break;
   default:
-    console.log("Not a valid day!");
+    console.log("Greate language too :D");
     break;
-}
-
-if (day === "monday") {
-  console.log("Plan course structure");
-  console.log("Go to coding meetup");
-} else if (day === "tuesday") {
-  console.log("Prepare theory videos");
-} else if (day === "wednesday" || day === "thursday") {
-  console.log("Write code examples");
-} else if (day === "friday") {
-  console.log("Record videos");
-} else if (day === "saturday" || day === "sunday") {
-  console.log("enjoy the weekend");
-} else {
-  console.log("Not a valid day!");
 }
