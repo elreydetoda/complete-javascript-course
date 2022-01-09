b83ec3cb89bf0403e097fdbf7ddb4626451136cf
elreydetoda <10230166+elreydetoda@users.noreply.github.com>
2022-01-09 21:01:26 +0000

    object methods + challenge before video

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -306,46 +306,79 @@
 // };
 // console.log(myCountry);
 
+// const jonas = {
+//   firstName: 'Jonas',
+//   lastName: 'Schmedtmann',
+//   age: 2037 - 1991,
+//   job: 'teacher',
+//   friends: ['Michael', 'Peter', 'Steven'],
+// };
+// console.log(jonas);
+// console.log(jonas.lastName);
+// console.log(jonas['lastName']);
+
+// const nameKey = 'Name';
+// console.log(jonas['first' + nameKey]);
+// console.log(jonas['last' + nameKey]);
+
+// // const interestedIn = prompt('What do you want to know about Jonas? (Choose between firstName, lastName, age, job, and friends)');
+// // if (jonas[interestedIn]) {
+// //   console.log(jonas[interestedIn]);
+// // } else {
+// //   console.log('Wrong request! (Choose between firstName, lastName, age, job, and friends)')
+// // }
+
+// jonas.location = 'Portugal';
+// jonas['twitter'] = '@jonasschmedtman';
+// console.log(jonas);
+
+// // challenge
+// console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+
+// // ASSIGNMENT
+
+// const myCountry = {
+//   country: 'USA',
+//   capital: 'Washington DC',
+//   language: 'english',
+//   population: 3441000000,
+//   neighbours: [ 'Mexico', 'Canada', ],
+// };
+// console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a captial called ${myCountry.capital}.`);
+// myCountry.population += 2000000
+// console.log(myCountry.population)
+// myCountry['population'] -= 2000000
+// console.log(myCountry.population)
+
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
-  age: 2037 - 1991,
+  birthYear: 1991,
   job: 'teacher',
   friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: true,
+  // calcAge: function(birthYear) {
+  //   return 2037 - birthYear
+  // }
+  // calcAge: function() {
+  //   return 2037 - this.birthYear
+  // }
+  calcAge: function() {
+    this.age = 2037 - this.birthYear
+    return this.age
+  },
+  // wasn't working for some reason `this` is referencing global (Window) object
+  // driversLicense: () => this //? 'a' : 'no'
+  driversLicense: function() {
+    return this.hasDriversLicense ? 'a' : 'no'
+  },
+  getSummary: function() {
+    return `${this.firstName} is a ${this.calcAge()}-year old teacher, and he has ${this.driversLicense() } driver's license`
+  }
 };
-console.log(jonas);
-console.log(jonas.lastName);
-console.log(jonas['lastName']);
 
-const nameKey = 'Name';
-console.log(jonas['first' + nameKey]);
-console.log(jonas['last' + nameKey]);
-
-// const interestedIn = prompt('What do you want to know about Jonas? (Choose between firstName, lastName, age, job, and friends)');
-// if (jonas[interestedIn]) {
-//   console.log(jonas[interestedIn]);
-// } else {
-//   console.log('Wrong request! (Choose between firstName, lastName, age, job, and friends)')
-// }
-
-jonas.location = 'Portugal';
-jonas['twitter'] = '@jonasschmedtman';
-console.log(jonas);
+// console.log(jonas.calcAge())
+// console.log(jonas.age)
 
 // challenge
-console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
-
-// ASSIGNMENT
-
-const myCountry = {
-  country: 'USA',
-  capital: 'Washington DC',
-  language: 'english',
-  population: 3441000000,
-  neighbours: [ 'Mexico', 'Canada', ],
-};
-console.log(`${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a captial called ${myCountry.capital}.`);
-myCountry.population += 2000000
-console.log(myCountry.population)
-myCountry['population'] -= 2000000
-console.log(myCountry.population)
+console.log(jonas.getSummary())
